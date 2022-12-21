ca641b8f3413ecdc64c44a7c562c7814e8738865
carreno17 <104216669+carreno17@users.noreply.github.com>
2022-12-21 13:36:42 -0400

    version 0.0.1

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py shell
-from djancrud.models import Categories
+from djangocrud.models import Categories
 Categories.objects.create(name="Ninguna")
+exit()
